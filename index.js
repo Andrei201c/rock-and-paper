43aa8a96733a554740d5c94fcd39814874cdeb5c
Andrei201c <andreiciuvat@gamil.com>
2022-10-06 21:29:24 +0300

    Edit index.js

--- index.js
+++ index.js
@@ -6,39 +6,18 @@ function getComputerChoice() {
         "Scissors"
     ];
     let item = items[Math.floor(Math.random() * items.length)]
-    item = item.toLowerCase();
-      return item;
+    item = item.toString().toLowerCase();
+  return item;  
+  }
+
+getComputerChoice();
+let playerscore = 0;
+let cpuscore = 0;
+let playerChoice = prompt("Enter your element: ").toString().toLowerCase();
+playerChoice = ' ';
+let computerChoice = getComputerChoice();
+computerChoice  = ' ';
+
+function playGround() {
+  computerChoice
 }
-
-let answerOne;
-let answerTwo;
-let playerChoice = prompt("Input one of Rock, scissors or Paper: ").toString().toLowerCase();
-const computerChoice = getComputerChoice();
-
-function playGround(playerChoice, computerChoice) {
-     
-    if ( playerChoice === computerChoice) {
-    console.log("tied");
-   } else if( (computerChoice == "scissors" &&  playerChoice == "rock" ) || 
-              (computerChoice == "rock" && playerChoice == "paper") ||
-              (computerChoice == "paper" && playerChoice == "scissors")){
-    answerOne = "You win! " + playerChoice + " beat " + computerChoice + "!";
-   return answerOne;
-     
-   } else {
-     answerTwo = "You lose! " + computerChoice + " beat " + playerChoice + "!";
-     return answerTwo;
-   }
-}
- 
-function game() {
-   for (i = 0; i < 5; i++){
-    playGround(playerChoice, computerChoice);
-   }
-}
-
-
-
-   
-
-console.log(playGround(playerChoice, computerChoice));
